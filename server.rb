fe2483dcfce95812c58399486e3a3e7b2b9a8372
Luis Carmona <50394301+Luisc013@users.noreply.github.com>
2019-12-17 10:52:10 -0500

    Updated env variable

--- server.rb
+++ server.rb
@@ -3,28 +3,19 @@ require './justdesserts.rb'
 require 'httparty'
 
 
-
-
-@@url = "https://api.spoonacular.com/recipes/search"
-
 get '/' do
   erb :index
 end
 
 get '/cakes' do
-  # req = HTTParty.get('https://api.spoonacular.com/recipes/search?query=cake&number=3&apiKey=889d85cffdfd45c58e3d6762bb4b446d')
-  # data = JSON.parse(req.body)
+
   erb :cakes
 end
 
 get '/cookies' do
-  # req = HTTParty.get('https://api.spoonacular.com/recipes/search?query=cookie&number=3&apiKey=889d85cffdfd45c58e3d6762bb4b446d')
-  # data = JSON.parse(req.body)
   erb :cookies
 end
 
 get '/muffins' do
-  # req = HTTParty.get('https://api.spoonacular.com/recipes/search?query=muffin&number=3&apiKey=889d85cffdfd45c58e3d6762bb4b446d')
-  # data = JSON.parse(req.body)
   erb :muffins
 end
