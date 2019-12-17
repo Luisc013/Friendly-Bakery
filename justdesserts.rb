09106f2e766882c42914895de20874b984ddd70d
Your Name <you@example.com>
2019-12-16 23:26:34 -0500

    styled class instances in each respective class route

--- justdesserts.rb
+++ justdesserts.rb
@@ -11,7 +11,7 @@ class Cookie < Dessert
       def initialize(name, image)
         super(name, image)
         @type = "Cookie"
-        @cost = 5.99
+        @cost = 27.99
     end
 end
 
@@ -21,7 +21,7 @@ class Cake < Dessert
       def initialize(name, image)
         super(name, image)
         @type = "Cake"
-        @cost = 19.99
+        @cost = 79.99
     end
 end
 
@@ -30,7 +30,7 @@ class Muffin < Dessert
       def initialize(name, image)
         super(name, image)
         @type = "Cupcakes"
-        @cost = 6.99
+        @cost = 46.99
   end
 end
 
@@ -44,8 +44,11 @@ gc = Cake.new("German Chocolate", "https://tatyanaseverydayfood.com/wp-content/u
 birthday = Cake.new("Birthday", "https://truffle-assets.imgix.net/671e1dd0-strawberry-dream-birthday-cake_pc.jpg")
 red_vel_c = Cake.new("Red Velvet", "https://www.modernhoney.com/wp-content/uploads/2017/12/Red-Velvet-Cake-12.jpg")
 flan = Cake.new("Flan", "https://tmbidigitalassetsazure.blob.core.windows.net/secure/RMS/attachments/37/1200x1200/Creamy-Caramel-Flan_exps2197_GPW2337930A12_09_2bC_RMS.jpg")
-fudge = Muffin.new("Fudge", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcUh5PPTikvR9ZAFS0TwMkIvQSR8Ezh-xuM69L3SwYtfv36_aE&s")
 red_vel_cc = Muffin.new("Red Velvet", "https://www.berries.com/blog/wp-content/uploads/2016/01/1-martha-sprinkles.jpg")
 oc = Muffin.new("Orange Cream", "https://www.berries.com/blog/wp-content/uploads/2016/01/14-gluten-free.jpg")
 uc = Muffin.new("Ultimate Chocolate", "https://www.berries.com/blog/wp-content/uploads/2016/01/29-pretty-simple-sweet.jpg")
 cheesecake_cc = Muffin.new("Cheesecake", "https://www.berries.com/blog/wp-content/uploads/2016/01/30-.jpg")
+
+@@cakes = [cheesecake_cake, gc, birthday, red_vel_c, flan]
+@@cookies = [choco_chip, red_vel, peanut_b, snickerdoodles, sugar]
+@@muffins = [red_vel_cc, oc, uc, cheesecake_cc]
